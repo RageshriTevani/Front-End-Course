661d4bd8415d8fb5a3b45f3443bd542d6db05036
RageshriTevani <147610242+RageshriTevani@users.noreply.github.com>
2024-06-13 18:29:39 +0530

    Add files via upload

---
+++ Reactjs/font-awesome-app/src/IconsApp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function IconsApp()
+{
+    return(
+        <>
+            <h1>register with us <span className="fa fa-map"></span></h1>
+            <h2></h2>
+        </>
+    )
+}
+export default IconsApp
---
+++ Reactjs/font-awesome-app/src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import IconsApp from "./IconsApp";
+import 'fontawesome-4.7'
+const root=ReactDOM.createRoot(document.getElementById("demo"));
+root.render(
+    <IconsApp />
+)
