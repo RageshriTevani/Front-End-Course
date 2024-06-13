70bafc8c21f21c658bd947d75d9a48dc52bbebb3
RageshriTevani <147610242+RageshriTevani@users.noreply.github.com>
2024-06-13 18:59:44 +0530

    Add files via upload

---
+++ Reactjs/aboutme-app/src/MyApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+function MyApp()
+{
+    return <p align='center' style={{fontWeight:"bolder",color:"black",backgroundColor:'lightgray',padding:'20px'}}>This is about me<br></br>
+    I am an optimistic person, always looking for an opportunity to learn something new and doing brainstorming things.<br></br>
+    I am a person who is always ready to help others no matter what<br></br>
+    Like to spare time for myself with reading some literatures, novels or self motivation books or making painting, playing musical instrument or listening old or new jams <br></br>
+    
+    </p>
+}
+export default MyApp
---
+++ Reactjs/aboutme-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MyApp from './MyApp';
+
+const root=ReactDOM.createRoot(document.getElementById("demo-app"));
+root.render(
+    <>
+    <h1 align='center' style={{backgroundColor:'black',color:"lightgrey",padding:'10px'}}>Hey I am Rageshri Tevani</h1>
+    <MyApp />
+    </>
+)
