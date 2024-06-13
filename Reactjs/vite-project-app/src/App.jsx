57aebadd5adb21ed258b960384e7baf94551f419
RageshriTevani <147610242+RageshriTevani@users.noreply.github.com>
2024-06-13 18:36:23 +0530

    Add files via upload

---
+++ Reactjs/vite-project-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from 'react-dom/client';
+import { Container } from "react-bootstrap";
+function App()
+{
+    return(
+        <>
+            <nav className=""></nav>
+        </>
+    )
+}
+export default App
