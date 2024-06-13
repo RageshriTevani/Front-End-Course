f49d984caa29d1cb32a1e8ae87cc74569479a137
RageshriTevani <147610242+RageshriTevani@users.noreply.github.com>
2024-06-13 18:27:47 +0530

    Add files via upload

---
+++ Reactjs/adv-js/events/events-window events/additions.js
@@ -0,0 +1,16 @@
+// function add()
+// {
+//     let a=10;
+//     let b=20;
+//     let c=a+b;
+//     document.write("Aditions of numbers is :"+c);
+// }
+
+
+window.onload=()=>{
+
+    let a=10;
+    let b=20;
+    let c=a+b;
+    document.write("Aditions of numbers is :"+c);
+    }
