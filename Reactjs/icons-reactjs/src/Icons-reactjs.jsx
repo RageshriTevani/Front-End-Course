ac7ab76b17171a8f2e7043ebae532f7dbb83ba7d
RageshriTevani <147610242+RageshriTevani@users.noreply.github.com>
2024-06-13 18:30:15 +0530

    Add files via upload

---
+++ Reactjs/icons-reactjs/src/Icons-reactjs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { FaArtstation } from "react-icons/fa"; 
+function Icon()
+{
+    return(
+        <>
+        <h1 align='center'>I like Art <FaArtstation/> </h1>
+        <h1 align='center'>I like Art <FaArtstation/> </h1>
+        <h1 align='center'>I like Art <FaArtstation/> </h1>
+        <h1 align='center'>I like Art <FaArtstation/> </h1>
+
+        </>
+    )
+}
+export default Icon
---
+++ Reactjs/icons-reactjs/src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import Icon from "./Icons-reactjs";
+const root=ReactDOM.createRoot(document.getElementById("demo"))
+root.render(
+    < Icon />
+
+)
