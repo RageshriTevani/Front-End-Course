eec53dbb842fd98085b66046815f55387056a545
RageshriTevani <147610242+RageshriTevani@users.noreply.github.com>
2024-06-13 18:48:50 +0530

    Add files via upload

---
+++ Reactjs/jsx-app/src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import Newelement from "./components/newelement";
+const root=ReactDOM.createRoot(document.getElementById("demo"));
+root.render(
+
+    <>
+        <Reactfundamentals />
+    </>
+
+)
