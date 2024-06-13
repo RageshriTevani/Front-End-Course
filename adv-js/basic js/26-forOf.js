a3b32a01515e288059b4a273543ebcbc1d75aabe
RageshriTevani <147610242+RageshriTevani@users.noreply.github.com>
2024-06-13 18:24:08 +0530

    Delete adv-js directory

--- adv-js/basic js/26-forOf.js
+++
@@ -1,8 +0,0 @@
-// The JavaScript for of statement loops through the values of an iterable object.
-
-// It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more:
-
-let numbers = [1, 2, 3, 4, 5];
-for (let number of numbers) {
-    console.log(number);
-}
